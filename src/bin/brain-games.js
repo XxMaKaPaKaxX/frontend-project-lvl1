13a37fd9ff6d0426170db937616c0e910f46592c
Max Lyovin <Lyovin.Maxim@gmail.com>
2020-01-04 16:07:40 +0100

    added readline-sync for conversation >> asking for player's name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import { playerGreeting } from '..';
+
 console.log('Welcome to the Brain Games!');
+playerGreeting();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const playerGreeting = () => {
+    const playerName = readlineSync.question('May I have your name?: ');
+    console.log('Hello, ' + playerName + '!');
+};
