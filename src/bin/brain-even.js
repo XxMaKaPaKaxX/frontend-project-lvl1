137ee54b7a4899576ece292757e7280e75e33869
Max Lyovin <Lyovin.Maxim@gmail.com>
2020-01-08 16:52:18 +0100

    brain-even game added

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { brainEvenGame } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+brainEvenGame();
--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@ import readlineSync from 'readline-sync';
 
 export const playerGreeting = () => {
   const playerName = readlineSync.question('May I have your name?: ');
-  console.log('Hello, ' + playerName + '!');
+  console.log(`Hello, ${playerName}!`);
+};
+
+export const brainEvenGame = () => {
+  const givRandomNumb = () => Math.floor(Math.random() * 100 + 1);
+  const isEven = (num) => ((num % 2) === 0 ? 'yes' : 'no');
+  let countOfCorrectAnswers = 0;
+  let numForGame;
+
+  const playerName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${playerName}!`);
+  while (countOfCorrectAnswers < 3) {
+    numForGame = givRandomNumb();
+    console.log(`Question: ${numForGame}`);
+    const playerAnswered = readlineSync.question('Your answer: ');
+    if (playerAnswered === isEven(numForGame)) {
+      console.log('Correct!');
+      countOfCorrectAnswers += 1;
+    } else {
+      console.log('No, try again');
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
 };
