a1f0b89f5d5d0d7e6823c7854e1d1d451a5853c6
Max Lyovin <Lyovin.Maxim@gmail.com>
2020-01-08 23:42:49 +0100

    brain-calc game added || prepare for reorganization

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { brainCalcGame } from '..';
+
+console.log('Welcome to the Brain Games!');
+brainCalcGame();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import readlineSync from 'readline-sync';
 
+
+const giveRandomNuber = (minimal, maximal) => {
+  const min = Math.ceil(minimal);
+  const max = Math.floor(maximal);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 export const playerGreeting = () => {
   const playerName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${playerName}!`);
 };
 
 export const brainEvenGame = () => {
-  const givRandomNumb = () => Math.floor(Math.random() * 100 + 1);
   const isEven = (num) => ((num % 2) === 0 ? 'yes' : 'no');
   let countOfCorrectAnswers = 0;
   let numForGame;
@@ -14,7 +20,7 @@ export const brainEvenGame = () => {
   const playerName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${playerName}!`);
   while (countOfCorrectAnswers < 3) {
-    numForGame = givRandomNumb();
+    numForGame = giveRandomNuber(1, 100);
     console.log(`Question: ${numForGame}`);
     const playerAnswered = readlineSync.question('Your answer: ');
     if (playerAnswered === isEven(numForGame)) {
@@ -26,3 +32,59 @@ export const brainEvenGame = () => {
   }
   console.log(`Congratulations, ${playerName}!`);
 };
+
+export const brainCalcGame = () => {
+  let countOfCorrectAnswers = 0;
+  const giveRandomOperatorInString = () => {
+    const randomNumber = giveRandomNuber(1, 3);
+    if (randomNumber === 1) {
+      return '+';
+    }
+    if (randomNumber === 2) {
+      return '-';
+    }
+    if (randomNumber === 3) {
+      return '*';
+    }
+  };
+
+  const calculation = (firstNumber, secondNumber, operator) => {
+    if (operator === '+') {
+      return firstNumber + secondNumber;
+    }
+    if (operator === '-') {
+      return firstNumber - secondNumber;
+    }
+    if (operator === '*') {
+      return firstNumber * secondNumber;
+    }
+  };
+
+  let firstRandomeNumber;
+  let secondRandomNumber;
+  let randomOperatorInString;
+  let resultForThisRoundInString;
+
+  const playerName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${playerName}!`);
+  console.log('What is the result of the expression?');
+
+  while (countOfCorrectAnswers < 3) {
+    firstRandomeNumber = giveRandomNuber(1, 100);
+    secondRandomNumber = giveRandomNuber(1, 100);
+    randomOperatorInString = giveRandomOperatorInString();
+    resultForThisRoundInString = String(calculation(firstRandomeNumber, secondRandomNumber, randomOperatorInString));
+    console.log(`Question: ${firstRandomeNumber} ${randomOperatorInString} ${secondRandomNumber}`);
+    const playerAnswered = readlineSync.question('Your answer: ');
+    if (resultForThisRoundInString === playerAnswered) {
+      console.log('Correct!');
+      countOfCorrectAnswers += 1;
+    } else {
+      console.log(`'${playerAnswered}' is wrong answer ;(. Correct answer was '${resultForThisRoundInString}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+// make the choice function >>>>>>
